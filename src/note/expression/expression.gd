e65f09c7b9440de6690d1194bf28444a74e07f75
xorblo-doitus <blabla86420@gmail.com>
2023-12-07 17:50:06 +0100

    Added primary script files for notes

---
+++ src/note/expression/expression.gd
@@ -0,0 +1,6 @@
+extends Note
+class_name Calculus
+
+
+func _init():
+	pass # Replace with function body.
---
+++ src/note/expression/variable/variable.gd
@@ -0,0 +1,6 @@
+extends Calculus
+class_name Variable
+
+
+func _init():
+	pass # Replace with function body.
