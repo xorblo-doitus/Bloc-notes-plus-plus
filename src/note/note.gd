f37a5a5054f18a103a4f1baf3b04c844dcdc939e
Halzun23 <118619893+Halzun23@users.noreply.github.com>
2023-12-08 09:04:06 +0000

    Ajouts de title et description à la classe note

--- src/note/note.gd
+++ src/note/note.gd
@@ -1,6 +1,11 @@
 extends RefCounted
 class_name Note
 
+## Texte principal de la note et celui qui est affiché dans la liste de note
+var title: String = ""
+## Texte descriptif plus étoffé que le titre
+var descripion: String = ""
+
 
 func _init():
-	pass # Replace with function body.
+	pass # Replace with function body
