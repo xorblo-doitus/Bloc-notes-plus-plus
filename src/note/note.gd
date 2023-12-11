c721fea28acd118319e5f0b4792184ea024df7d2
Halzun23 <118619893+Halzun23@users.noreply.github.com>
2023-12-11 16:25:36 +0000

    Started coding serialization

--- src/note/note.gd
+++ src/note/note.gd
@@ -4,11 +4,28 @@ class_name Note
 ## Texte principal de la note et celui qui est affiché dans la liste de note
 var title: String = ""
 ## Texte descriptif plus étoffé que le titre
-var descripion: String = ""
+var description: String = ""
 ## Des données, comme un texte ou un nombre...utiles pour la note
 ## [br] key: [String] value: [Variant]
 var other_data: Dictionary = {}
 
+## Renvoie un texte pour pouvoir sauvegarder facilement dans un .txt
+func serialize() -> String:
+	var data: Dictionary = {
+		"title": title, 
+		"description": description, 
+		"other data": other_data
+		}
+	return JSON.stringify(data, "\t")
 
-func _init():
-	pass # Replace with function body
+## Désérialise une note sauvegardée en texte
+static func deserialise(saved: String) -> Note:
+	var new_note := Note.new()
+	var json := JSON.new()
+	
+	json.parse(saved)
+	
+	return new_note
+	
+	
+const attributes_to_serialize: Array[StringName] = [&"title", &"description", &"other_data"]
