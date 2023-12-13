10a5bacb44ba5d9b643e6a738ac33abdfea15828
Halzun23 <118619893+Halzun23@users.noreply.github.com>
2023-12-13 15:01:22 +0000

    Modification de la classe Variable

--- src/note/calculus/variable/variable.gd
+++ src/note/calculus/variable/variable.gd
@@ -1,6 +1,9 @@
 extends Calculus
 class_name Variable
 
+## Permet à l'utilisateur d'entrer un nom de variable 
+## qui servira de référence pour d'autres calculs
+var name: String = ""
 
 func _init():
 	pass # Replace with function body.
