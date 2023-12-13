8c139d1fe5a5335a7e5d8278c568416c01a83b20
Halzun23 <118619893+Halzun23@users.noreply.github.com>
2023-12-13 14:47:12 +0000

    Ajout attribut Calculus

--- src/note/calculus/calculus.gd
+++ src/note/calculus/calculus.gd
@@ -1,6 +1,11 @@
 extends Note
 class_name Calculus
 
-
 func _init():
 	pass # Replace with function body.
+
+func get_value() -> float:
+	var expression = Expression.new()
+	expression.parse(title)
+	var result = expression.execute()
+	return result
