289a32fb861d39ffb69ba9454b41f6e7931bbdd7
xorblo-doitus <blabla86420@gmail.com>
2024-01-01 10:11:35 +0100

    Added missing attribute to Variable

--- src/note/calculus/variable/variable.gd
+++ src/note/calculus/variable/variable.gd
@@ -2,5 +2,9 @@ extends Calculus
 class_name Variable
 
 
+## A unique name allowing to reference this variable from anywhere in the notes.
+var name: String = "x"
+
+
 func _init():
 	pass # Replace with function body.
