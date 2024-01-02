6d0663cdd84797f6cc316d7ce4972dad6450e49b
xorblo-doitus <blabla86420@gmail.com>
2024-01-02 12:27:23 +0100

    Update from template

--- src/singletons/wait.gd
+++ src/singletons/wait.gd
@@ -12,3 +12,12 @@ func secondes(time: float) -> void:
 
 func ms(time: float) -> void:
 	await secondes(time/1000)
+
+
+func process_frames(frames: int = 1) -> void:
+	for __ in frames:
+		await tree.process_frame
+
+func physics_frames(frames: int = 1) -> void:
+	for __ in frames:
+		await tree.physics_frame
