946655790d2c012852c267eb5a2f4098e882a643
xorblo-doitus <blabla86420@gmail.com>
2024-01-04 17:53:32 +0100

    Fixed Note according to test errors

--- src/note/note.gd
+++ src/note/note.gd
@@ -2,12 +2,20 @@ extends RefCounted
 class_name Note
 
 ## Texte principal de la note et celui qui est affiché dans la liste de note
-var title: String = ""
+var title: String = "":
+	set(new):
+		title = new
+		for display in _connected_displays:
+			display.title = title
 func set_title(new: String) -> Note:
 	title = new
 	return self
 ## Texte descriptif plus étoffé que le titre
-var description: String = ""
+var description: String = "":
+	set(new):
+		description = new
+		for display in _connected_displays:
+			display.description = description
 ## Des données, comme un texte ou un nombre...utiles pour la note
 ## [br] key: [String] value: [Variant]
 var other_data: Dictionary = {}
@@ -24,7 +32,7 @@ func apply_to_display(display: NoteDisplay) -> void:
 	
 	_connected_displays.append(display)
 	
-	display._connections.append(Connection.new(display.title_changed, set_title, true))
+	display._connections.append(Connection.new(display.title_changed, set_title.unbind(1), true))
 
 
 func unapply_to_display(display: NoteDisplay) -> void:
