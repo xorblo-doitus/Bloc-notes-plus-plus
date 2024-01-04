d6b8bc309827c41bcd2c53b27407e6577b4fd0c4
Halzun23 <118619893+Halzun23@users.noreply.github.com>
2024-01-04 06:24:38 +0100

    Update note_builder.gd

--- src/note_builder.gd
+++ src/note_builder.gd
@@ -9,7 +9,7 @@ var description: String = ""
 var other_data: Dictionary = {}
 
 var data: Dictionary = {}
-
+var note_number = 0
 var type: Object = Note
 #func _init(text:="") -> void:
 	#if text == "1" :
@@ -33,17 +33,23 @@ func parse(text: String):
 	if len(splited) == 2:
 		title = splited[0]
 		description = splited[1]
+	note_number += 1
+	if progression == 100:
+		progression = 0
 
 var prefixes = ["/", "\\"]
+var finished_task = 0
 
 func commande(text: String):
 	if text[0] in prefixes:
-		if text[1] == "t" or text.substr(1, 4) == "task":
+		if text[1] == "t" or text.substr(1, 2) == "ta" or text.substr(1, 3) == "tas" or text.substr(1, 4) == "task":
 			type = Task
 			return text.substr(2)
-		elif text[1] == "c" or text.substr(1, 4) == "calc":
+		elif text[1] == "c" or text.substr(1, 2) == "ca" or text.substr(1, 3) == "cal" or text.substr(1, 4) == "calc":
 			type = Calculus
 			return text.substr(2)
-		elif text[1] == "s" or text.substr(1, 5) == "store":
+		elif text[1] == "s" or text.substr(1, 2) == "st"  or text.substr(1, 3) == "sto"  or text.substr(1, 4) == "stor"  or text.substr(1, 5) == "store":
 			var res = Variable
 			return text.substr(2)
+
+var progression = note_number*100/finished_task
