19f493f57eb76a16f95fa874d86ca631e678f65f
xorblo-doitus <blabla86420@gmail.com>
2024-01-04 10:06:55 +0100

    Remove useless echo

--- tasks/branch_helpers/rebase.cmd
+++ tasks/branch_helpers/rebase.cmd
@@ -2,7 +2,6 @@ for /f %%i in ('git rev-parse --abbrev-ref HEAD') do set branch=%%i
 
 if %branch% == main echo Cannot rebase main on main. & exit 1
 
-echo oups
 
 git stash push --include-untracked
 git fetch
