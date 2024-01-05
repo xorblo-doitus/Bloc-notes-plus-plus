6fa0091d92d26c0290d1445859839164084046ca
xorblo-doitus <blabla86420@gmail.com>
2024-01-05 17:18:09 +0100

    Created a scene to manually test NoteListDisplay

---
+++ test/manual/test_note_list_display.gd
@@ -0,0 +1,13 @@
+extends Control
+
+
+
+func _ready() -> void:
+	var notes: Array[Note] = [
+		Note.new("Première note", "Une description succinte"),
+		Note.new(),
+		Note.new("Bah, la note précédente n'a pas de titre ?!", "(C'est pour tester.)")
+	]
+	
+	
+	$NoteListDisplay.notes = notes
