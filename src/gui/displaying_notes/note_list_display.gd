57195461c0a9d683adb248d2660cc359a554a5fb
xorblo-doitus <blabla86420@gmail.com>
2024-01-05 15:51:11 +0100

    Implemented `NoteListDisplay.instantiate()`

--- src/gui/displaying_notes/note_list_display.gd
+++ src/gui/displaying_notes/note_list_display.gd
@@ -1,3 +1,7 @@
 class_name NoteListDisplay
 extends PanelContainer
 
+
+static func instantiate() -> NoteListDisplay:
+	var new: NoteListDisplay = preload("res://src/gui/displaying_notes/note_list_display.tscn").instantiate()
+	return new
