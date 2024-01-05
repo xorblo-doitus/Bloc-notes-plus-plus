83d120a0f446ad7cd826d96a16463b134465e231
xorblo-doitus <blabla86420@gmail.com>
2024-01-05 17:29:52 +0100

    Fix error when reading the same node to tree

--- src/singletons/shrinker.gd
+++ src/singletons/shrinker.gd
@@ -7,6 +7,7 @@ func _ready() -> void:
 		bind(control)
 		
 	get_tree().node_added.connect(_on_node_added)
+	get_tree().node_removed.connect(_on_node_removed)
 
 
 func _on_node_added(node: Node) -> void:
@@ -17,8 +18,21 @@ func _on_node_added(node: Node) -> void:
 			assert(false, "A non-Control node is in `shrink` group.")
 
 
+func _on_node_removed(node: Node) -> void:
+	if node.is_in_group(&"shrink"):
+		if node is Control:
+			unbind(node)
+		else:
+			assert(false, "A non-Control node is in `shrink` group.")
+
+
 func bind(control: Control) -> void:
 	control.resized.connect(shrink.bind(control))
+
+
+func unbind(control: Control) -> void:
+	# Works even if it's not really the same callable due to binding.
+	control.resized.disconnect(shrink)
 
 
 func shrink(control: Control) -> void:
