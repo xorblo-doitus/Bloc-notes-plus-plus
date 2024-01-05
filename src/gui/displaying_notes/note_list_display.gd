329ed894cfaeaa24117f22af1ec45c1e4281512e
xorblo-doitus <blabla86420@gmail.com>
2024-01-05 17:16:53 +0100

    Fixed preload cyclic dependency

--- src/gui/displaying_notes/note_list_display.gd
+++ src/gui/displaying_notes/note_list_display.gd
@@ -16,8 +16,14 @@ var notes: Array[Note]:
 	get:
 		return note_list.notes
 
+## Stores displays used by this list displayer.
+var _note_displays: Array[NoteDisplay] = []
+
+
+## A reference to the default packed scene associated with this class
+static var _default_scene: PackedScene = load("res://src/gui/displaying_notes/note_list_display.tscn")
 
 ## Create a new instance by loading default scene for this class.
 static func instantiate() -> NoteListDisplay:
-	var new: NoteListDisplay = preload("res://src/gui/displaying_notes/note_list_display.tscn").instantiate()
-	return new
+	return _default_scene.instantiate()
+
