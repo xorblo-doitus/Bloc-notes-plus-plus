7390b27e1c4f410acfd2595a0dcbac05b215ffb3
xorblo-doitus <blabla86420@gmail.com>
2024-01-05 17:18:31 +0100

    trailing comma

--- test/manual/test_note_list_display.gd
+++ test/manual/test_note_list_display.gd
@@ -6,7 +6,7 @@ func _ready() -> void:
 	var notes: Array[Note] = [
 		Note.new("Première note", "Une description succinte"),
 		Note.new(),
-		Note.new("Bah, la note précédente n'a pas de titre ?!", "(C'est pour tester.)")
+		Note.new("Bah, la note précédente n'a pas de titre ?!", "(C'est pour tester.)"),
 	]
 	
 	
