16983d135b6f9a337b0d30f465e3a746e6c7c230
xorblo-doitus <blabla86420@gmail.com>
2024-01-07 08:42:10 +0100

    Moved `DEFAULT_NOTES` to WorkspaceSave

--- src/app/main.gd
+++ src/app/main.gd
@@ -1,7 +1,6 @@
 extends Control
 
 
-static var DEFAULT_NOTES: Array[Note] = [Note.new("Exemple de note.", "Ceci est une description.")]
 @export var notes_display: NoteListDisplay
 
 
--- src/app/workspace_save.gd
+++ src/app/workspace_save.gd
@@ -5,9 +5,19 @@ extends RefCounted
 ## Stores informations about a workspace (Notes, sorting method...)
 
 
+## The default value of [member note_list]
+static var DEFAULT_NOTES: NoteList = NoteList.new([
+	Note.new("Exemple de note.", "Ceci est une description."),
+])
+
+
 ## The notes in this workspacce.
 ## [br][br][b]READONLY[/b]: Trying to modifying this attribute will only set it's
 ## [member NoteList.notes] to a [i]copy[/i] of the new one's [member NoteList.notes]
-var note_list: NoteList = NoteList.new():
+var note_list: NoteList = NoteList.new().mimic(DEFAULT_NOTES):
 	set(new):
 		note_list.mimic(new)
+
+
+func _init() -> void:
+	pass
