4f10f2ac32db88fe54cb31e7df4641920f17e65a
xorblo-doitus <blabla86420@gmail.com>
2024-01-08 10:56:34 +0100

    Added Saver

--- src/saving/saver.gd
+++ src/saving/saver.gd
@@ -5,7 +5,16 @@ extends Object
 
 
 static func save_object(target: Object, path: String) -> void:
+	DirAccess.make_dir_recursive_absolute(path.get_base_dir())
+	
 	var file: FileAccess = FileAccess.open(path, FileAccess.WRITE)
+	
+	if not file:
+		assert(
+			false,
+			"Saving at `" + path + "` failed due to error n°" + str(FileAccess.get_open_error())
+			+ " (" + error_string(FileAccess.get_open_error()) + ")"
+		)
 	
 	file.store_string(Serializer.serialize(target))
 	
