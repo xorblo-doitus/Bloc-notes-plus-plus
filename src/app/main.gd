2f234a3b512297240a62a4f105c2deb690191c14
xorblo-doitus <blabla86420@gmail.com>
2024-01-09 08:44:20 +0100

    Improved main app with workspace loading error handling

--- src/app/main.gd
+++ src/app/main.gd
@@ -6,7 +6,12 @@ extends Control
 
 var workspace: WorkspaceSave = WorkspaceSave.new():
 	set(new):
+		if new == null:
+			assert(false, "Trying to assign null to workspace wich is a non nullable attribute.")
+			return
 		_disconnect_from_workspace(workspace)
+		_connect_to_workspace(new)
+		workspace = new
 
 
 
@@ -20,10 +25,13 @@ func load_workspace() -> void:
 	)
 	
 	if loaded_workspace is ErrorHelper:
-		loaded_workspace.popup()
-		return
-	
-	workspace = loaded_workspace
+		if not loaded_workspace.godot_builtin_error == ERR_FILE_NOT_FOUND:
+			loaded_workspace.popup()
+		
+		# Triger setter
+		workspace = workspace
+	else:
+		workspace = loaded_workspace
 	
 	_on_workspace_changed()
 
@@ -37,6 +45,9 @@ func _connect_to_workspace(target: WorkspaceSave) -> WorkspaceSave:
 
 ## Chainable
 func _disconnect_from_workspace(target: WorkspaceSave) -> WorkspaceSave:
+	if not target.changed.is_connected(_on_workspace_changed):
+		return target
+	
 	target.changed.disconnect(_on_workspace_changed)
 	
 	return target
