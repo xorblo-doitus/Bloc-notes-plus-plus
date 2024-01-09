fbe0d766704d853b316bdd9a73c98f0811aa1d27
xorblo-doitus <blabla86420@gmail.com>
2024-01-09 18:36:35 +0100

    Implemented Variable

--- src/note/calculus/variable/variable.gd
+++ src/note/calculus/variable/variable.gd
@@ -1,14 +1,37 @@
-extends Calculus
 class_name Variable
+extends Calculus
+
 
 ## Permet à l'utilisateur d'entrer un nom de variable 
 ## qui servira de référence pour d'autres calculs.
 var name: String = ""
 
 
-func _init():
-	pass # Replace with function body.
+## Stores a reference to all alive variables
+static var all: Array[Variable] = []
 
+
+static func get_all_var_for_calculus() -> Dictionary:
+	var result: Dictionary = {}
+	
+	for variable in all:
+		result[variable.name] = variable.get_value()
+	
+	return result
+
+
+static func _static_init() -> void:
+	Calculus.variable_getters.append(Variable.get_all_var_for_calculus)
+
+
+func _init():
+	all.append(self)
+
+
+func _notification(what: int) -> void:
+	match what:
+		NOTIFICATION_PREDELETE:
+			all.erase(self)
 
 
 func _is_equal(other: Variant) -> bool:
