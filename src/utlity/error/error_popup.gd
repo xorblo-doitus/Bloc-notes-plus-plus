f5eaf976214286b404c49e9b929e10a30ad4646f
xorblo-doitus <blabla86420@gmail.com>
2024-01-09 08:42:09 +0100

    Added ErrorPopup

---
+++ src/utlity/error/error_popup.gd
@@ -0,0 +1,14 @@
+#class_name ErrorPopup
+extends AcceptDialog
+
+
+func _init() -> void:
+	hide()
+
+
+### A reference to the default packed scene associated with this class
+#static var _default_scene: PackedScene = load("res://src/utlity/error/error_popup.tscn")
+#
+### Create a new instance by loading default scene for this class.
+#static func instantiate() -> ErrorPopup:
+	#return _default_scene.instantiate()
