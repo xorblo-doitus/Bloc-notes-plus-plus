c4e99d595ff5c177b31b757627376ead55ca7e6c
xorblo-doitus <blabla86420@gmail.com>
2024-01-12 08:21:17 +0100

    Notes can be moved around the screen (without changing their place)

---
+++ src/gui/user_input/grip_componenent.gd
@@ -0,0 +1,74 @@
+class_name GripComponent
+extends Control
+
+
+## A componnent allowing to drag a GUI element.
+
+@export var element_to_move: Control
+@export var right_click_to_cancel: bool = true
+@export_group("offset")
+## If true, [member offset] will be automatically defined so that starting dragging
+## does not teleport the GUI to the mouse. Else [member offset] is left unchanged.
+@export var auto_offset: bool = true
+@export var offset: Vector2 = Vector2.ZERO
+
+var _dragging: bool = false
+#var _starting_mouse_global_position: Vector2 = Vector2(-1, -1)
+## Automatically add the element to tree and free it when unreferenced.
+var _ghost_element: Control:
+	set(new):
+		if _ghost_element:
+			get_tree().root.remove_child(_ghost_element)
+			_ghost_element.queue_free()
+		if new:
+			get_tree().root.add_child(new)
+		_ghost_element = new
+
+
+func _ready() -> void:
+	set_process(false)
+
+
+func _process(_delta) -> void:
+	update_ghost_position()
+
+
+func _input(event: InputEvent) -> void:
+	if event.is_action(&"cancel_ongoing_behavior"):
+		abort_dragging()
+
+
+func start_dragging() -> void:
+	if _dragging:
+		return
+	
+	if element_to_move == null:
+		push_warning("No element to drag.")
+		return
+	
+	_dragging = false
+	
+	if auto_offset:
+		offset = get_global_mouse_position() - element_to_move.global_position
+	
+	_ghost_element = element_to_move.duplicate(0)
+	DisplayServer.cursor_set_shape(DisplayServer.CURSOR_DRAG)
+	
+	set_process(true)
+	
+	#_starting_mouse_global_position = get_global_mouse_position()
+
+
+func finish_dragging() -> void:
+	abort_dragging()
+
+
+func abort_dragging() -> void:
+	DisplayServer.cursor_set_shape(DisplayServer.CURSOR_ARROW)
+	_ghost_element = null
+	set_process(false)
+	_dragging = false
+
+
+func update_ghost_position() -> void:
+	_ghost_element.global_position = get_global_mouse_position() + offset
