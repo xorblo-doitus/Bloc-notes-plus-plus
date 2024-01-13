0805514c4a4de3ee5360a0718ac3a2439f5f5c37
Halzun23 <118619893+Halzun23@users.noreply.github.com>
2024-01-13 15:18:19 +0000

    Suppression de commande() et parse() dans Note_Builder

--- src/note_creation/note_builder.gd
+++ src/note_creation/note_builder.gd
@@ -9,7 +9,6 @@ var description: String = ""
 var other_data: Dictionary = {}
 
 var data: Dictionary = {}
-var note_number = 0
 var type: Object = Note
 var time_limit: int = 0
 #func _init(text:="") -> void:
@@ -25,37 +24,3 @@ func build():
 	var new_note: Note = type.new()
 	for key in data:
 		new_note.set(key, data[key])
-		
-
-func parse(text: String):
-	text = text.strip_edges()
-	commande(text)
-	var splited: Array[String] = text.split("\n", true, 1)
-	if len(splited) == 2:
-		title = splited[0]
-		description = splited[1]
-	note_number += 1
-	if progression == 100:
-		progression = 0
-	data = {}
-    data["name"] = title
-	if type = Task:
-		data["time_limit"] = time_limit
-    
-
-var prefixes = ["/", "\\"]
-var finished_task = 0
-
-func commande(text: String):
-	if text[0] in prefixes:
-		if text[1] == "t" and text.substr(2, 4) == "   " or text.substr(1, 2) == "ta" and text.substr(3, 4) == "  " or text.substr(1, 3) == "tas" and text[3] == " " or text.substr(1, 4) == "task":
-			type = Task
-			return text.substr(2)
-		elif text[1] == "c" and text.substr(2, 4) == "   " or text.substr(1, 2) == "ca"  and text.substr(3, 4) == "  " or text.substr(1, 3) == "cal" and text[3] == " " or text.substr(1, 4) == "calc":
-			type = Calculus
-			return text.substr(2)
-		elif text[1] == "s" and text.substr(2, 5) == "    " or text.substr(1, 2) == "st" and text.substr(3, 5) == "   " or text.substr(1, 3) == "sto"  and text.substr(4, 5) == "  " or text.substr(1, 4) == "stor" text[4] == " " or text.substr(1, 5) == "store":
-			var res = Variable
-			return text.substr(2)
-
-var progression = note_number*100/finished_task
