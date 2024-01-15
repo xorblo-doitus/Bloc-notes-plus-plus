81156fe84d4ded733dd60045e4fcc83ae1737904
xorblo-doitus <blabla86420@gmail.com>
2024-01-15 18:39:27 +0100

    Refactored NoteDiplay to remove duplicate attributes between classes

--- src/gui/displaying_notes/note_display/note_display.gd
+++ src/gui/displaying_notes/note_display/note_display.gd
@@ -5,28 +5,13 @@ extends PanelContainer
 ## Emitted when delete button is pressed, etc...
 signal request_remove()
 
-## Texte principal de la note et celui qui est affiché dans la liste de note.
-var title: String = "":
-	set(new):
-		if new == title:
-			return
-		title = new
-		%Title.text = new
-
-## Texte descriptif plus étoffé que le titre.
-var description: String = "":
-	set(new):
-		if new == description:
-			return
-		description = new
-		%Title.tooltip_text = new
-
 
 @warning_ignore("unused_private_class_variable")
 var _connections: Array[Connection]
 var _displaying: Note
 
 
+@onready var title: EditableRichTextLabel = %Title
 @onready var grip_component: GripComponent = %GripComponent
 
 
@@ -43,13 +28,17 @@ static func instantiate() -> NoteDisplay:
 
 
 func display(note: Note) -> void:
-	title = note.title
-	description = note.description
-	
-	_connections.append(Connection.new(note.title_changed, _on_note_title_changed, true))
-	_connections.append(Connection.new(note.description_changed, _on_note_description_changed, true))
+	if not is_node_ready():
+		ready.connect(display.bind(note), CONNECT_ONE_SHOT)
+		return
 	
 	_displaying = note
+	
+	_on_note_title_changed()
+	_on_note_description_changed()
+	
+	_connections.append(Connection.new(note.title_changed, _on_note_title_changed.unbind(2), true))
+	_connections.append(Connection.new(note.description_changed, _on_note_description_changed.unbind(2), true))
 
 
 func undisplay() -> void:
@@ -62,17 +51,16 @@ func undisplay() -> void:
 	_displaying = null
 
 
-func _on_note_title_changed(new: String, _old: String) -> void:
-	title = new
+func _on_note_title_changed() -> void:
+	title.text = _displaying.title
 
 
-func _on_note_description_changed(new: String, _old: String) -> void:
-	description = new
+func _on_note_description_changed() -> void:
+	title.tooltip_text = _displaying.description
 
 
 func _on_title_text_changed(new: String, _old: String):
-	#_displaying.title = new
-	pass
+	_displaying.title = new
 
 
 func _on_delete_pressed() -> void:
