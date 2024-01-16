6204e91c6d4e1c9839235a568ed277181462fbe6
xorblo-doitus <blabla86420@gmail.com>
2024-01-16 12:19:09 +0100

    Reworked ErrorHelper

--- src/utlity/error/error_helper.gd
+++ src/utlity/error/error_helper.gd
@@ -13,10 +13,36 @@ func _init(_title: String = "", _description: String = "", _godot_builtin_error:
 	godot_builtin_error = _godot_builtin_error
 
 
+## Chainable
+func set_title(new: String) -> ErrorHelper:
+	title = new
+	return self
+
+
+## Chainable
+func set_description(new: String) -> ErrorHelper:
+	description = new
+	return self
+
+
+## Chainable
+func set_godot_builtin_error(new: Error) -> ErrorHelper:
+	godot_builtin_error = new
+	return self
+
+
 ## Craetes a popup displaying this error.
 func popup() -> void:
 	#var error_popup: ErrorPopup = ErrorPopup.instantiate()
-	ErrorPopup.title = title
-	ErrorPopup.dialog_text = description
-	ErrorPopup.popup()
+	var title_elements: Array[String] = []
+	if title:
+		title_elements.append(title)
+	if godot_builtin_error != OK:
+		title_elements.append(tr("ERROR_STRING").format({
+			"godot_builtin_error": godot_builtin_error,
+			"error_as_text": error_string(godot_builtin_error)
+		}))
+	ErrorPopup.set_title_elements(title_elements)
+	ErrorPopup.dialog_text = description if description else TranslationServer.tr(&"ERROR_WITHOUT_DESCRIPTION")
+	ErrorPopup.show()
 	
