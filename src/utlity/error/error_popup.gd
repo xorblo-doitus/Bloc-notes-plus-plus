5b42c5d667a86197069d01141683b557d4c6f1ac
xorblo-doitus <blabla86420@gmail.com>
2024-01-16 12:18:22 +0100

    Added a utility method to ErrorPopup

--- src/utlity/error/error_popup.gd
+++ src/utlity/error/error_popup.gd
@@ -4,3 +4,15 @@ extends AcceptDialog
 
 func _init() -> void:
 	hide()
+
+
+func set_title_elements(elements: Array[String] = []) -> void:
+	# Add the software title so the user know that the error come from us.
+	elements.push_front(
+		EasySettings.get_setting("application/config/name_localized").get(
+			OS.get_locale_language(),
+			EasySettings.get_setting("application/config/name")
+		)
+	)
+	
+	title = " - ".join(elements)
