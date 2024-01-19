7d2bce7b99bda7bb8d59ed2f7f3e5675039d5ebd
xorblo-doitus <blabla86420@gmail.com>
2024-01-19 08:01:59 +0100

    Fix tests accordingly to new EditableRichTextLabel behavior

--- test/unit/test_inputs.gd
+++ test/unit/test_inputs.gd
@@ -10,31 +10,33 @@ func test_editable_rich_text_label() -> void:
 	
 	watch_signals(editable_rich_text_label)
 	
-	var sender = InputSender.new(editable_rich_text_label)
+	#var sender = InputSender.new(editable_rich_text_label)
 	var double_click: InputEventMouse = InputFactory.mouse_double_click(Vector2.ZERO)
 	double_click.pressed = true
 	
 	assert_eq(
-		editable_rich_text_label.get_text_stored(),
+		editable_rich_text_label.text,
 		"Start",
-		"get_text_stored() does not return the right text."
+		"text is not the initial text."
 	)
 	
 	assert_false(editable_rich_text_label.editing)
-	sender.send_event(double_click)
+	#sender.send_event(double_click)
+	editable_rich_text_label.code_edit.grab_focus()
 	assert_true(editable_rich_text_label.editing)
 	
 	assert_eq(
-		editable_rich_text_label.get_text_stored(),
+		editable_rich_text_label.text,
 		"Start",
-		"get_text_stored() does not return the right text."
+		"text changed by edition without input."
 	)
 	
-	editable_rich_text_label.get_node("%CodeEdit").text = "hello"
+	editable_rich_text_label.editing = true
+	editable_rich_text_label.code_edit.text = "hello"
 	
 	assert_signal_not_emitted(editable_rich_text_label, &"text_changed", "Emited but text did not change.")
 	
-	var sender2 = InputSender.new(editable_rich_text_label.get_node("%CodeEdit"))
+	var sender2 = InputSender.new(editable_rich_text_label.code_edit)
 	var validate: InputEventKey = InputFactory.key_down(KEY_ENTER)
 	validate.shift_pressed = true
 	sender2.send_event(validate)
@@ -51,15 +53,15 @@ func test_editable_rich_text_label() -> void:
 	)
 	
 	assert_eq(
-		editable_rich_text_label.get_text_stored(),
+		editable_rich_text_label.text,
 		"hello",
-		"get_text_stored() does not return the right text."
+		"text not updated by editing."
 	)
 	
 	assert_ne(
-		editable_rich_text_label.get_text_stored(),
+		editable_rich_text_label.text,
 		"hello\n",
-		"Validating create a \\n."
+		"Validating create a \\n (line break)."
 	)
 	
 	editable_rich_text_label.free()
