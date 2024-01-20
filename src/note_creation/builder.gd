811898dda9ef8cc950f3d96483f0f7aa1ee22449
xorblo-doitus <blabla86420@gmail.com>
2024-01-20 10:30:26 +0100

    Implemented Builder

--- src/note_creation/builder.gd
+++ src/note_creation/builder.gd
@@ -1,4 +1,4 @@
-class_name NoteBuilder
+class_name Builder
 extends RefCounted
 
 
@@ -8,23 +8,51 @@ extends RefCounted
 ## tout en laissant la possibilité de changer le type final de la [Note].
 
 
-## Texte principal de la [Note] et celui qui est affiché dans la liste de note
-var title: String = ""
-## Texte descriptif plus étoffé que le titre
-var description: String = ""
-## Des données, comme un texte ou un nombre...utiles pour la note
-## [br] key: [String] value: [Variant]
-var other_data: Dictionary = {}
+### Texte principal de la [Note] et celui qui est affiché dans la liste de note
+#var title: String = ""
+### Texte descriptif plus étoffé que le titre
+#var description: String = ""
+### Des données, comme un texte ou un nombre...utiles pour la note
+### [br] key: [String] value: [Variant]
+#var other_data: Dictionary = {}
 
-var data: Dictionary = {}
+
+## The type of object to be created.
 var type: Object = Note
-var time_limit: int = 0
-#func _init(text:="") -> void:
-	#if text == "1" :
-		#var json = JSON.new()
-		#text = json.parse()
+## The attributes values of the future new object.
+var attributes: Dictionary = {}
 
-func build():
-	var new_note: Note = type.new()
-	for key in data:
-		new_note.set(key, data[key])
+
+
+func build() -> Object:
+	var infos: BuildingInfo = BuildingInfo.get_most_precise(type)
+	var new: Object = infos.instantiating_function.call()
+	
+	apply_to_existing(new)
+	
+	return new
+
+
+func edit_object(object: Object, use_object_current_config: bool = true) -> void:
+	type = object.get_script()
+	
+	if use_object_current_config:
+		var infos: BuildingInfo = BuildingInfo.get_most_precise(type)
+		
+		attributes.clear()
+		attributes.merge(object.get_meta(&"_builder_unused_attributes", {}))
+		
+		for attribute in infos.attributes_to_edit:
+			attributes[attribute] = object.get(attribute)
+
+
+func apply_to_existing(object: Object) -> void:
+	var unused_attributes: Dictionary = {}
+	
+	for attribute in attributes:
+		if attribute in object:
+			object.set(attribute, attributes[attribute])
+		else:
+			unused_attributes[attribute] = attributes[attribute]
+	
+	object.set_meta(&"_builder_unused_attributes", unused_attributes)
