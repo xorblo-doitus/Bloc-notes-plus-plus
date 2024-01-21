23af60317a1961e8edbb9e6e42d5c65415aaaa47
xorblo-doitus <blabla86420@gmail.com>
2024-01-21 13:32:34 +0100

    Added a keybind to toggle `always_on_top`

--- src/singletons/global_inputs.gd
+++ src/singletons/global_inputs.gd
@@ -4,7 +4,7 @@ extends Node
 
 
 func _process(_delta) -> void:
-	if Input.is_action_just_pressed("fullscreen"):
+	if Input.is_action_just_pressed(&"fullscreen"):
 		# Get opposite state as current
 		var now_fullscreen = get_window().mode != Window.MODE_FULLSCREEN
 		
@@ -13,3 +13,7 @@ func _process(_delta) -> void:
 			get_window().mode = Window.MODE_FULLSCREEN
 		else:
 			get_window().mode = Window.MODE_WINDOWED
+	
+	if Input.is_action_just_pressed(&"always_on_top"):
+		get_window().always_on_top = not get_window().always_on_top
+		print("set always_on_top to ", get_window().always_on_top)
