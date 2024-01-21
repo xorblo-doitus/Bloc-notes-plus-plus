f57dd9091955cb1e9a59acab519a9280dc4b4ad3
xorblo-doitus <blabla86420@gmail.com>
2024-01-21 08:38:29 +0100

    Added `ErrorHelper._to_string()`

--- src/utlity/error/error_helper.gd
+++ src/utlity/error/error_helper.gd
@@ -31,6 +31,20 @@ func set_godot_builtin_error(new: Error) -> ErrorHelper:
 	return self
 
 
+func _to_string() -> String:
+	var result: String = tr(&"ERROR_PREFIX") + title
+	if godot_builtin_error:
+		result += " - " + tr("ERROR_STRING").format({
+			"godot_builtin_error": godot_builtin_error,
+			"error_as_text": error_string(godot_builtin_error)
+		})
+	
+	if description:
+		result += "\n" + description
+	
+	return result
+
+
 ## Craetes a popup displaying this error.
 func popup() -> void:
 	#var error_popup: ErrorPopup = ErrorPopup.instantiate()
