62a5ce80d340f994b284ecac89336f69d87b9f49
xorblo-doitus <blabla86420@gmail.com>
2024-01-24 19:23:55 +0100

    Implemented implicit commands so that title and description can be set without using commands

--- src/note_creation/parser.gd
+++ src/note_creation/parser.gd
@@ -8,15 +8,35 @@ static var global_scope := {}
 static var prefixes = ["/", "\\"]
 static var quick_entry = Builder
 
+
+static func _static_init() -> void:
+	Command.new().set_names(
+		["flush_unused_tokens"]
+	).set_callback(
+		Parser.flush_unused_tokens
+	).build()
+
+
+static func flush_unused_tokens(_argument: Array[String], context: Dictionary) -> void:
+	var text: String = " ".join(context["_unused_tokens"]).strip_edges()
+	var builder: Builder = context["_current_builder"]
+	if builder.attributes.get("title"):
+		builder.set_attribute("description", builder.attributes.get("description", "") + text)
+	else:
+		builder.set_attribute("title", text)
+
+
 static func parse():
 	reset_global_scope()
-	
+	print(tokens)
 	#if len(splited) == 2:
 		#Builder.title = splited[0]
 		#Builder.description = splited[1]
 	#for token in tokens:
 	while not tokens.is_empty():
 		var token: String = tokens.pop_front()
+		var was_a_command: bool = false
+		
 		for prefix in prefixes:
 			if token.begins_with(prefix):
 				var command_name: String = token.trim_prefix(prefix)
@@ -26,14 +46,23 @@ static func parse():
 						for __ in len(command.parameters):
 							arguments.append(tokens.pop_front())
 						command.callback.call(arguments, global_scope)
+						
+						was_a_command = true
+						break
 				break
-			
+		if not was_a_command:
+			global_scope["_unused_tokens"].append(token)
 
-static func tokenise(text) -> Array[String]:
+
+static func tokenise(text: String) -> Array[String]:
 	tokens = []
-	var splited: PackedStringArray = text.split(" ", true, 1)
+	
+	text = text.replace("\n", "\n " + prefixes[0] + "flush_unused_tokens ")
+	
+	var splited: PackedStringArray = text.split(" ", true)
 	
 	tokens.append_array(splited)
+	tokens.append(prefixes[0] + "flush_unused_tokens")
 	
 	return tokens
 
@@ -42,6 +71,8 @@ static func reset_global_scope():
 	global_scope.clear()
 	
 	global_scope["_current_builder"] = Builder.new()
+	global_scope["_unused_tokens"] = []
+
 
 static func execute(text: String):
 	tokenise(text)
