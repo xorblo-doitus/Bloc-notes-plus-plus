242f19c00a16e6fceee4d79b80fc20e8636c16e5
xorblo-doitus <blabla86420@gmail.com>
2024-01-24 19:22:21 +0100

    Added `Command.set_parameters()`

--- src/note_creation/command.gd
+++ src/note_creation/command.gd
@@ -1,23 +1,35 @@
 class_name Command
 extends RefCounted
 
+
 static var all: Array[Command] = []
+
+
 var names: Array[String] = []
-var parameters = []
+var parameters: Array[String] = []
 var callback: Callable
+
 
 enum ParseError {
 	OK,
 	NOT_ENOUGH_ARGUMENT,
 }
 
+
 func set_names(new_names: Array[String]) -> Command:
 	names = new_names
 	return self
- 
+
+
+func set_parameters(new_parameters: Array[String]) -> Command:
+	parameters = new_parameters
+	return self
+
+
 func build():
 	Command.all.append(self)
 	return self
+
 
 func set_callback(function: Callable):
 	callback = function
