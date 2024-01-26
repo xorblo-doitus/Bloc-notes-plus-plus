38538216d661009b84d5a3472716bc07009a39b4
xorblo-doitus <blabla86420@gmail.com>
2024-01-26 18:29:18 +0100

    User can note create (only basic) notes

--- src/app/workspace_save.gd
+++ src/app/workspace_save.gd
@@ -26,12 +26,3 @@ static var DEFAULT_NOTES: NoteList = NoteList.new([
 var note_list: NoteList = NoteList.new().mimic(DEFAULT_NOTES):
 	set(new):
 		note_list.mimic(new)
-
-
-
-#func _init() -> void:
-	#note_list.changed.connect(_on_note_list_changed)
-
-
-#func _on_note_list_changed(_new: Array[Note], _old: Array[Note]) -> void:
-	#changed.emit()
