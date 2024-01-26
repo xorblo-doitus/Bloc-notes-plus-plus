a0590fb7fdaa282c3518883e239959a4cbe451f4
xorblo-doitus <blabla86420@gmail.com>
2024-01-26 18:31:32 +0100

    Moved date related attribute because it's not only related to tasks

--- src/note/task/task.gd
+++ src/note/task/task.gd
@@ -2,10 +2,6 @@ class_name Task
 extends Note
 
 
-## The date at wich the Task must be done.
-## 0 meaning there is no time limit
-var time_limit: int = 0
-
 ## Wether or not the task is done.
 ## Revertable.
 var done: bool = false
