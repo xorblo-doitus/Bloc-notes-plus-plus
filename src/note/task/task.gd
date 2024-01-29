8fdc5dd288a6d9c56d574f2ec794d309f7da1923
xorblo-doitus <blabla86420@gmail.com>
2024-01-29 19:03:04 +0100

    Added a done_toggled signal to Task

--- src/note/task/task.gd
+++ src/note/task/task.gd
@@ -2,10 +2,23 @@ class_name Task
 extends Note
 
 
+signal done_toggled(new: bool)
+
+
 ## Wether or not the task is done.
 ## Revertable.
-var done: bool = false
+var done: bool = false:
+	set(new):
+		if new == done:
+			return
+		
+		done = new
+		done_toggled.emit(new)
 
+
+func set_done(new: bool) -> Task:
+	done = new
+	return self
 
 
 func _is_equal(other: Variant) -> bool:
