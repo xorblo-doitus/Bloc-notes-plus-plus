ea3b811cc23412429d9ab305912890cb96664bb3
Halzun23 <118619893+Halzun23@users.noreply.github.com>
2024-01-29 16:15:03 +0000

    Update default commands

--- src/note_creation/default_commands.gd
+++ src/note_creation/default_commands.gd
@@ -9,13 +9,30 @@ static func _static_init():
 static func create_defaults():
 	Command.new().set_names(
 		["store", "s"]
-	).set_parameters(
+).set_parameters(
 		["variable_name"]
 	).set_callback(
 		DefaultCommands.store_callback
 	).build()
-
+	
+	Command.new().set_names(
+		["task", "t"]
+	).set_callback(
+		DefaultCommands.task_callback
+	).build()
+	
+	Command.new().set_names(
+		["calc", "c"]
+	).set_callback(
+		DefaultCommands.calc_callback
+	).build()
 
 static func store_callback(argument: Array[String], context: Dictionary):
 	context["_current_builder"].type = Variable
 	context["_current_builder"].set_attribute("name", argument[0])
+	
+static func task_callback(argument: Array[String], context: Dictionary):
+	context["_current_builder"].type = Task
+
+static func calc_callback(argument: Array[String], context: Dictionary):
+	context["_current_builder"].type = Calculus
