b755f804b8150c89541fbcc48f7a2e24bed6bf53
xorblo-doitus <blabla86420@gmail.com>
2024-01-29 07:30:36 +0100

    Use the new ErrorButton in CalculusResult

--- src/gui/displaying_notes/note_display/widgets/calculus_result.gd
+++ src/gui/displaying_notes/note_display/widgets/calculus_result.gd
@@ -5,7 +5,7 @@ extends DisplayWidget
 static var equal_string = " ="
 
 @onready var result: Label = %Result
-@onready var error_button: TextureButton = %ErrorButton
+@onready var error_button: ErrorButton = %ErrorButton
 
 
 func _connect_to(_note: Note) -> void:
@@ -17,15 +17,11 @@ func _connect_to(_note: Note) -> void:
 
 func update_value() -> void:
 	if note.error:
-		error_button.tooltip_text = str(note.error)
+		error_button.error = note.error
+		#error_button.tooltip_text = str(note.error)
 		result.add_theme_color_override(&"font_color", get_theme_color(&"font_placeholder_color", &"LineEdit"))
-		error_button.show()
+		#error_button.show()
 	else:
 		result.text = str(note.value) + WidgetCalculusResult.equal_string
 		result.remove_theme_color_override(&"font_color")
-		error_button.hide()
-
-
-func _on_error_button_pressed() -> void:
-	if note.error:
-		note.error.popup()
+		error_button.error = null
