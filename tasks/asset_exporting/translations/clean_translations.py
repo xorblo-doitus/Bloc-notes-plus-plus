4cd79ff8b5335238cb38c7cd8db23a78cf32a205
xorblo-doitus <blabla86420@gmail.com>
2024-01-29 12:16:52 +0100

    Fix CSV translation cleaning for multiline translations

--- tasks/asset_exporting/translations/clean_translations.py
+++ tasks/asset_exporting/translations/clean_translations.py
@@ -1,23 +1,32 @@
+import csv
+
 cleaned = []
 
-with open("src/assets/translations/translations.csv", "r", encoding="utf-8") as file:
-	first_line = file.readline().rstrip()
+with open("src/assets/translations/translations.csv", "r", encoding="utf-8", newline="") as file:
+	reader = csv.reader(file)
+	
+	first_line = next(reader)
+	
 	to_crop = 0
-	for char in first_line[::-1]:
-		if char == ",":
+	for header in first_line[::-1]:
+		if header == "":
 			to_crop += 1
 		else:
 			break
 	
 	if to_crop > 0:
-		for row in [first_line] + file.readlines():
-			cleaned.append(row.rstrip()[:-to_crop])
+		for row in [first_line] + list(reader):
+			cleaned.append(row[:-to_crop])
 	else:
-		cleaned = [first_line] + file.readlines()
+		cleaned = None
 
-with open("src/assets/translations/translations.csv", "w", encoding="utf-8") as file:
-	for i, row in enumerate(cleaned):
-		if row.endswith("\n"):
-			continue
-		cleaned[i] += "\n"
-	file.writelines(cleaned)
+if not cleaned is None:
+	with open("src/assets/translations/translations.csv", "w", encoding="utf-8", newline="") as file:
+		writer = csv.writer(file)
+		# for row in cleaned:
+		writer.writerows(cleaned)
+		# for i, row in enumerate(cleaned):
+		# 	if row.endswith("\n"):
+		# 		continue
+		# 	cleaned[i] += "\n"
+		# file.writelines(cleaned)
