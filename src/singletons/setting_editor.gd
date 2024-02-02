63e6728b13fd245dbf6ae3ef92ad69a910ecacaf
xorblo-doitus <blabla86420@gmail.com>
2024-02-02 18:23:33 +0100

    Workaround to show validate on the right

--- src/singletons/setting_editor.gd
+++ src/singletons/setting_editor.gd
@@ -5,6 +5,10 @@ func _ready() -> void:
 	hide()
 	size = 0.7 * DisplayServer.screen_get_size()
 	size.x /= 2
+	
+	var HBox: HBoxContainer = get_ok_button().get_parent()
+	HBox.move_child(get_ok_button(), 2)
+	HBox.move_child(get_cancel_button(), 1)
 
 
 
