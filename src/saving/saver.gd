f5ff3e4271f467ded25ea474194bfc1657debf67
xorblo-doitus <blabla86420@gmail.com>
2024-02-02 08:01:33 +0100

    Use ErrorHelper in `Saver.save_object()`

--- src/saving/saver.gd
+++ src/saving/saver.gd
@@ -2,19 +2,28 @@ class_name Saver
 extends Object
 
 
-static func save_object(target: Object, path: String) -> void:
+static func save_object(target: Object, path: String) -> ErrorHelper:
 	DirAccess.make_dir_recursive_absolute(path.get_base_dir())
 	
 	var file: FileAccess = FileAccess.open(path, FileAccess.WRITE)
 	
 	if not file:
-		assert(
-			false,
-			"Saving at `" + path + "` failed due to error n°" + str(FileAccess.get_open_error())
-			+ " (" + error_string(FileAccess.get_open_error()) + ")"
+		var error: ErrorHelper = ErrorHelper.new().set_title(
+			TranslationServer.tr(&"ERROR_SAVE_FAILED")
+		).set_godot_builtin_error(
+			FileAccess.get_open_error()
 		)
+		
+		return error
+		#assert(
+			#false,
+			#"Saving at `" + path + "` failed due to error n°" + str(FileAccess.get_open_error())
+			#+ " (" + error_string(FileAccess.get_open_error()) + ")"
+		#)
 	
 	file.store_string(Serializer.serialize(target))
+	
+	return null
 
 
 ## If something goes wrong, returns [code]null[/code]
@@ -23,7 +32,7 @@ static func load_object_from_file(path: String) -> Object:
 	
 	if file == null:
 		var error: ErrorHelper = ErrorHelper.new().set_title(
-			TranslationServer.tr(&"LOAD_FAILED")
+			TranslationServer.tr(&"ERROR_LOAD_FAILED")
 		).set_godot_builtin_error(
 			FileAccess.get_open_error()
 		)
