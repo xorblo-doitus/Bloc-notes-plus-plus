2636b6eac26a1fbc63df3e57d7111dacc721c248
xorblo-doitus <blabla86420@gmail.com>
2024-02-02 08:04:01 +0100

    Added a global file dialog

---
+++ src/singletons/global_file_dialog.gd
@@ -0,0 +1,33 @@
+extends FileDialog
+
+
+var in_use: bool = false:
+	set(new):
+		in_use = new
+		if not in_use:
+			Connection.destroy_all(_connections)
+
+
+func _ready() -> void:
+	size = 0.5 * DisplayServer.screen_get_size()
+
+
+func request_dialog() -> bool:
+	if in_use:
+		return false
+	
+	in_use = true
+	return true
+
+
+var _connections: Array[Connection] = []
+
+## [param connection] must not be already connected.
+func connect_while_in_use(connection: Connection) -> void:
+	connection.create()
+	_connections.append(connection)
+
+
+func _on_visibility_changed() -> void:
+	if not visible:
+		in_use = false
